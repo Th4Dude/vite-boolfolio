805be748cf646fb6a85d45866cb50dee69ca82b2
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-25 19:48:01 +0300

    chiamta api

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,54 @@
 <script>
 
+import axios from 'axios';
 import AppFooter from './components/AppFooter.vue';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 export default {
   name:'App',
+  data() {
+
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api',
+      apiUrls: {
+        projects:'/projects'
+      },
+      projects: []
+    }
+
+  },
   components: {
+
     AppFooter,
     AppHeader,
     AppMain,
+
+  },
+  methods: {
+
+      getProjects(){
+        axios.get(this.apiBaseUrl + this.apiUrls.projects).then((response)=>{
+          this.projects = response.data;
+          /* console.log(response); */
+        }).catch((error)=>{
+          console.log(error);
+        })
+      }
+  },
+  created() {
+    this.getProjects();
   }
 }
 
 </script>
 
 <template>
+
     <AppHeader></AppHeader>
-    <AppMain> </AppMain> 
+    <AppMain :data="projects" />
     <AppFooter></AppFooter>
+
 </template>
 
 
