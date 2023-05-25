f5049ed9d04a937a138811e785905ab7ca724b2e
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-25 20:09:57 +0300

    results in view

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,15 +11,17 @@ export default{
  
 
 <template>
+    
     <div class="container">
         <div class="row">
            <div class="col" v-for="project in data">
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">{{project.title}}</h5>
+                        <p class="card-text">{{project.description}}</p>
+                        <p class="card-text">{{project.slug}}</p>
+                        <a href="#" class="btn btn-primary">Don't try this at home!</a>
                     </div>
                 </div>
            </div>  
