04aa075a14dd34d53c95e1c8e85c15184c4305ec
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-26 20:25:30 +0300

    chiamta  + view

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -11,17 +11,15 @@ export default {
  
 
 <template>
-
-   
-            <div class="card" style="width: 18rem;">
-                <img v-if="project.image" :src="project.image" class="card-img-top" alt="...">
-                <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ project.description }}</p>
-                <p class="card-text">{{ project.slug }}</p>
-                <a href="#" class="btn btn-primary">Don't try this at home!</a>
-            </div>
-   
-
+    <div class="card" style="width: 18rem;">
+        <img v-if="project.image" :src="project.image" class="card-img-top" alt="...">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p class="card-text">{{ project.description }}</p>
+        <p class="card-text">{{ project.slug }}</p>
+        <router-link :to="{ name: 'list', params: { slug: project.slug } }" class="btn btn-primary">
+            try it
+        </router-link>
+    </div>
 </template>
 
 
--- src/pages/UnknownPage.vue
+++ src/pages/UnknownPage.vue
@@ -1,20 +1,49 @@
 <script>
 
-
+import axios from 'axios';
 export default {
-  name:'UnknownPage',
+    name: 'UnknownPage',
+    data() {
+        return {
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            apiUrls: {
+                projects: '/projects'
+            },
+            project: null
+        }
+    },
+    methods: {
+        getProject() {
+            axios.get(this.apiBaseUrl + this.apiUrls.projects + '/' + this.$route.params.slug)
+                .then((response) => {
+                    this.project = response.data.results;
+                })
+        }
+    },
+    created() {
+        this.getProject();
+    }
 
 }
 
 </script>
 
 <template>
-    
-    <h1>ciao</h1>
+
+    <div class="container">
+        <div class="row ">
+            <div class="col d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img v-if="project.image" :src="project.image" class="card-img-top" alt="...">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                    <p class="card-text">{{ project.slug }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 
-<style>
-
-</style>
+<style></style>
