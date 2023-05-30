c83844223d9612b02f36796fd435e5229f7137df
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-30 17:13:53 +0300

    form contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -8,7 +8,19 @@ export default {
 
 <template>
 
-    <h1>contatti</h1>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name and surname">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+        <button type="button" class="btn btn-secondary">Send</button>
 
 </template>
 
