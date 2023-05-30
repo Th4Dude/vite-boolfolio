857366d6cbc842c235b7526a10918da31ffc1ee0
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-30 18:58:53 +0300

    axios chiamata

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,26 +1,60 @@
 <script>
 
+import axios from 'axios';
+import store from '../store';
+
+
 export default {
-    name: 'ContactPage'
+    name: 'ContactPage',
+    data(){
+        return {
+           store,
+           formData : {
+            name: '',
+            email: '',
+            message: ''
+           } 
+        }
+    },
+    methods: {
+            sendContact(){
+                const data = {
+                    name: this.formData.name,
+                    email: this.formData.email,
+                    message: this.formData.message,
+                };
+                axios.post(`$this.store.apiBaseUrl}/contacts`, data)
+                .then((response)=> {
+                    console.log(response);
+                }).catch (( error )=> {
+                    console.log(error);
+            });
+        }
+    }
 }
 
 </script>
 
 <template>
 
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Name</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name and surname">
+    <form @submit.prevent="sendContact">
+
+        <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name"  placeholder="Name and surname" v-model="formData.name">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email"  placeholder="name@example.com" v-model="formData.email">
     </div>
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <label for="message" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="message" rows="3" v-model="formData.message"></textarea>
     </div>
         <button type="button" class="btn btn-secondary">Send</button>
+    
+    </form>
+    
 
 </template>
 
